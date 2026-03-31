db75436a9a08871ac68718768742f5aaa8c32641
Simon Levermann <github@simon.slevermann.de>
2026-03-31 16:32:49 +0200

    feat(org): add DOMAIN_REJECT_PLUS_ADDRESSING constant to OrganizationModel

--- server-spi/src/main/java/org/keycloak/models/OrganizationModel.java
+++ server-spi/src/main/java/org/keycloak/models/OrganizationModel.java
@@ -31,6 +31,7 @@ public interface OrganizationModel {
     String ORGANIZATION_DOMAIN_ATTRIBUTE = "kc.org.domain";
     String ALIAS = "alias";
     String HIDE_IDP_ON_LOGIN_WHEN_ORGANIZATION_UNKNOWN = "kc.org.broker.login.hide-when-org-unknown";
+    String DOMAIN_REJECT_PLUS_ADDRESSING = "kc.org.domain.reject-plus-addressing";
 
     enum IdentityProviderRedirectMode {
         EMAIL_MATCH("kc.org.broker.redirect.mode.email-matches");
